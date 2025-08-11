f4704846cf51d7be3e3944f0a580c7a21c998611
iammessier <nahatajay6@gmail.com>
2025-08-11 19:03:37 +0530

    webapp is made

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.exception import CustomException
 from src.logger import logging
 from sklearn.metrics import r2_score
 
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -28,5 +29,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             model_report[model_name] = r2_square
             logging.info(f"{model_name} R2 Score: {r2_square}")
         return model_report
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
